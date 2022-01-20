f0c6a93ed9c5dde3eab3dcb5396e886823cfefea
lucas <lucasucb2020@gmail.com>
2022-01-20 11:04:29 -0300

    Exemplo básico de conexão

--- main.py
+++ main.py
@@ -15,10 +15,25 @@ Inserindo dados numa tabela do banco
 
 cursor.execute('INSERT INTO disciplina values("pedro")')
 
+Lendo dados de uma tabela
+
+
+cursor.execute("SELECT * FROM disciplina")
+cursor.execute("SELECT * FROM disciplina")
+
+dados = cursor.fetchall()
+
+
+print(dados)
 
 """
 
 
 cursor = db.cursor()
 
-cursor.execute('INSERT INTO disciplina values("pedro")')
+cursor.execute("SELECT * FROM disciplina")
+
+dados = cursor.fetchall()
+
+
+print(dados)
